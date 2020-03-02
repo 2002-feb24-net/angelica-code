41bd07bf682e371da55e75310beced690716929a
Angelica Velez <anvelez26@gmail.com>
2020-03-02 14:13:47 -0600

    fizzbuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int value = RunProgram();
+         int input = int.Parse(Console.ReadLine());
+        
+        }
+
+        static int RunProgram()
+        {
+            for(int i = 0; i<1000; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    int f = System.Console.WriteLine("Fizz");
+                    return f;
+                    
+                }
+                else if(i % 5 == 0)
+                {
+                   int b = System.Console.WriteLine("Buzz");
+                   return b;
+                    
+                }
+                else if(i % 3 == 0 && i % 5 ==0)
+                {
+                    int fb = System.Console.WriteLine("FizzBuzz");
+                     return fb;
+
+                }
+
+            }
+        }
+
+        
+
+
+    }
+}
---
+++ acronym/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+
+namespace acronym
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = getMultiTerm();
+            char[] acro = InterpretStringAsArray(input);
+            //PrintArray(array);
+           
+
+        }
+
+        static string getMultiTerm()
+        {
+            Console.WriteLine("Please a multi-word term: ");
+            Console.WriteLine("i.e 'Central Processing Unit'");
+            string multiTerm = Console.ReadLine();
+            return multiTerm;
+        }
+
+         static string[] InterpretStringAsArray(string str)
+        {
+            string[] parts = str.Split(' ');
+            string[] acro = parts.Substring();
+            for (int i = 0; i < acro.Length; i++)
+            {
+                string letter = acro[i];
+                Console.Write("Acronym: " + letter);
+                
+            }
+            return letter;
+        }
+
+        //  static void PrintArray(int[] a)
+        // {
+        //     Console.Write("Acronym: ");
+
+        //     for (int i = 0; i < a.Length; i++)
+        //     {
+        //         string numAsString = a[i].ToString();
+        //         Console.Write(numAsString + " ");
+        //     }
+        //     Console.WriteLine();
+        // }
+    
+    }
+}
